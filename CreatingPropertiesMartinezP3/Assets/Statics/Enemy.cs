28136b498b0a18dc21c0003da4d4142f4eba708e
Diego Martinez <145380966+E1Pancakes@users.noreply.github.com>
2025-03-28 14:02:01 -0700

    organizeing the scenes and scripts

--- CreatingPropertiesMartinezP3/Assets/Statics/Enemy.cs
+++ CreatingPropertiesMartinezP3/Assets/Statics/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy 
 {
     //Static variables are shared across all instances
     //of a class.
